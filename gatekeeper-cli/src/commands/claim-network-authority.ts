1134abc237559f6471e6265ba7dd2bbe93f96cb6
Robert Leonard <rleonard@Roberts-MacBook-Pro.local>
2024-06-21 07:45:14 -0400

    Add cli method to transfer + claim primary authority of a network

---
+++ gatekeeper-cli/src/commands/claim-network-authority.ts
@@ -0,0 +1,59 @@
+import {Command, Flags} from '@oclif/core'
+
+import {makeGatewayNetworkTs} from '../utils/oclif/utils'
+import {
+  confirmationsFlag,
+  feesFlag, gatekeeperNetworkFlag,
+  gatewayTokenAddressFlag,
+  chainFlag, parseFlagsWithPrivateKey,
+  privateKeyFlag, gasLimitFlag,
+  gatewayNetworkAddressFlag,
+} from '../utils/oclif/flags'
+import {addressArg} from '../utils/oclif/args'
+
+export default class ClaimNetworkAuthority extends Command {
+  static description = 'Transfer network primary authority';
+
+  static examples = [
+    `$ gateway-eth add-network-authority 0x893F4Be53274353CD3379C87C8fd1cb4f8458F94 -n 123
+		`,
+  ];
+
+  static flags = {
+    help: Flags.help({char: 'h'}),
+    privateKey: privateKeyFlag(),
+    gatewayTokenAddress: gatewayTokenAddressFlag(),
+    gatekeeperNetwork: gatekeeperNetworkFlag(),
+    gatewayNetworkAddress: gatewayNetworkAddressFlag(),
+    chain: chainFlag(),
+    fees: feesFlag(),
+    gasLimit: gasLimitFlag(),
+    confirmations: confirmationsFlag(),
+  };
+
+  static args = [
+    {name: 'networkName', required: true, description: 'Name of the network'}
+  ];
+  async run(): Promise<void> {
+    const {args, flags} = await this.parse(ClaimNetworkAuthority)
+
+    const confirmations = flags.confirmations
+
+    const authority = args.address as string
+    const networkName = args.networkName as string;
+    const parsedFlags = parseFlagsWithPrivateKey(flags)
+
+    this.log(`Adding:
+			authority ${authority}
+			to network ${parsedFlags.gatekeeperNetwork}`)
+
+      const gatewayNetwork = await makeGatewayNetworkTs(parsedFlags)
+    const sendableTransaction = await gatewayNetwork.claimPrimaryAuthority(networkName);
+
+    const receipt = await sendableTransaction.wait(confirmations)
+
+    this.log(
+      `Added network authority to Gateway Token contract. TxHash: ${receipt.transactionHash}`,
+    )
+  }
+}
--- gatekeeper-cli/src/commands/transfer-network-authority.ts
+++ gatekeeper-cli/src/commands/transfer-network-authority.ts
@@ -1,17 +1,18 @@
 import {Command, Flags} from '@oclif/core'
 
-import {makeGatewayTs} from '../utils/oclif/utils'
+import {makeGatewayNetworkTs} from '../utils/oclif/utils'
 import {
   confirmationsFlag,
   feesFlag, gatekeeperNetworkFlag,
   gatewayTokenAddressFlag,
   chainFlag, parseFlagsWithPrivateKey,
   privateKeyFlag, gasLimitFlag,
+  gatewayNetworkAddressFlag,
 } from '../utils/oclif/flags'
 import {addressArg} from '../utils/oclif/args'
 
-export default class AddNetworkAuthority extends Command {
-  static description = 'Add a network authority to a GatewayToken contract';
+export default class TransferNetworkAuthority extends Command {
+  static description = 'Transfer network primary authority';
 
   static examples = [
     `$ gateway-eth add-network-authority 0x893F4Be53274353CD3379C87C8fd1cb4f8458F94 -n 123
@@ -23,27 +24,32 @@ export default class AddNetworkAuthority extends Command {
     privateKey: privateKeyFlag(),
     gatewayTokenAddress: gatewayTokenAddressFlag(),
     gatekeeperNetwork: gatekeeperNetworkFlag(),
+    gatewayNetworkAddress: gatewayNetworkAddressFlag(),
     chain: chainFlag(),
     fees: feesFlag(),
     gasLimit: gasLimitFlag(),
     confirmations: confirmationsFlag(),
   };
 
-  static args = [addressArg({description: 'Network authority address to add to the gatekeeper network'})];
+  static args = [
+    addressArg({description: 'Primary authority address to transfer network ownership'}),
+    {name: 'networkName', required: true, description: 'Name of the network'}
+  ];
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(AddNetworkAuthority)
+    const {args, flags} = await this.parse(TransferNetworkAuthority)
 
     const confirmations = flags.confirmations
 
     const authority = args.address as string
+    const networkName = args.networkName as string;
     const parsedFlags = parseFlagsWithPrivateKey(flags)
 
     this.log(`Adding:
 			authority ${authority}
 			to network ${parsedFlags.gatekeeperNetwork}`)
 
-    const gateway = await makeGatewayTs(parsedFlags)
-    const sendableTransaction = await gateway.addNetworkAuthority(authority, parsedFlags.gatekeeperNetwork)
+      const gatewayNetwork = await makeGatewayNetworkTs(parsedFlags)
+    const sendableTransaction = await gatewayNetwork.updatePrimaryAuthority(networkName, authority);
 
     const receipt = await sendableTransaction.wait(confirmations)
 
