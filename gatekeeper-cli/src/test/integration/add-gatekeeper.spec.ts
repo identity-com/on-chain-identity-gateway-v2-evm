ff2a218d2dd5fb83baa2714d23c42724b1b9dc3f
Robert Leonard <rleonard@Roberts-MacBook-Pro.local>
2024-06-25 16:44:15 -0400

    add gatekeeper test

--- gatekeeper-cli/src/test/integration/add-gatekeeper.spec.ts
+++ gatekeeper-cli/src/test/integration/add-gatekeeper.spec.ts
@@ -1,6 +1,6 @@
 import { BaseProvider } from "@ethersproject/providers";
 import * as dotenv from "dotenv";
-import { ethers, Wallet } from "ethers";
+import { ethers, utils, Wallet } from "ethers";
 import {runCommand} from '@oclif/test'
 import { BNB_TESTNET_CONTRACT_ADDRESSES, createRandomString, FOUNDRY_DEFAULT_WALLET_ONE, GatewayNetworkClient } from "../../utils";
 import assert = require('assert');
@@ -11,19 +11,21 @@ dotenv.config();
 describe("Command: Create network", function () {
     let provider: BaseProvider;
     let testWallet: Wallet;
+    let gatekeeper: Wallet;
     let networkClient: GatewayNetworkClass;
 
-    before("Initialize gateway gatekeeper ts class", async function () {
+    before("Initialize wallet", async function () {
         this.timeout(20000);
         provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545"); // Test run against local node forked
 
         testWallet = FOUNDRY_DEFAULT_WALLET_ONE.connect(provider);
+        gatekeeper = Wallet.createRandom();
         networkClient = GatewayNetworkClient(testWallet, BNB_TESTNET_CONTRACT_ADDRESSES.gatewayNetwork)
     });
 
-    it("should create new gateway network", async function () {
+    it("add a new gatekeeper to a network", async function () {
         const networkName = createRandomString(8)
-        const result = await runCommand([
+        await runCommand([
             "create-gatekeeper-network", 
             networkName,
             `${testWallet.address}`,
@@ -36,14 +38,23 @@ describe("Command: Create network", function () {
             `--chain=localhost`
         ])
 
+        const result = await runCommand([
+            "add-gatekeeper",
+            gatekeeper.address,
+            networkName,
+            `--privateKey=${testWallet.privateKey}`,
+            `--gatewayNetworkAddress=${BNB_TESTNET_CONTRACT_ADDRESSES.gatewayNetwork}`,
+            `--chain=localhost`
+        ])
+
         console.log(`output: ${result.stdout}`)
 
-        const wasTxConfirmed = result.stdout.includes("Created Gatekeeper Network. TxHash:")
+        const wasTxConfirmed = result.stdout.includes("Added gatekeeper to Gateway Token contract. TxHash:")
         assert.equal(wasTxConfirmed, true, "Transaction should be confirmed on node")
         assert.equal(result.error, undefined, "No errors should occur when creating network")
 
-        const answer = await networkClient.getNetworkId(networkName);
+        const answer = await networkClient.isGatekeeper(utils.formatBytes32String(networkName), gatekeeper.address);
 
-        assert.notEqual(answer, 0, "networkId should be non zero")
+        assert.equal(answer, true, "networkId should be non zero")
     })
 })
