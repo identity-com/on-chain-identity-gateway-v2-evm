1f574b55ef18173ce741bcdbdc967685470cfbbc
Robert Leonard <leaonrd.robert.h@gmail.com>
2024-07-08 17:00:16 -0400

    add command to allow gatekeeper to issue tokens on a given network

---
+++ gatekeeper-cli/src/commands/issue-token.ts
@@ -0,0 +1,73 @@
+import {
+    confirmationsFlag,
+    feesFlag, gatekeeperNetworkFlag,
+    gatewayTokenAddressFlag,
+    chainFlag, parseFlagsWithPrivateKey,
+    privateKeyFlag, gasLimitFlag,
+    gatewayNetworkAddressFlag,
+  } from '../utils/oclif/flags'
+  import {Args, Command, Flags} from '@oclif/core'
+  import {makeGatewayNetworkTs, makeGatewayTs} from '../utils/oclif/utils'
+  import { utils, Wallet } from 'ethers';
+  
+  export default class AddGatekeeper extends Command {
+    static description = 'Gatekeepers can issue tokens on their respective networks';
+  
+    static examples = [
+      `$ gateway-eth issue-token 0x893F4Be53274353CD3379C87C8fd1cb4f8458F94 -n 123
+          `,
+    ];
+  
+    static flags = {
+      help: Flags.help({char: 'h'}),
+      privateKey: privateKeyFlag(),
+      gatewayTokenAddress: gatewayTokenAddressFlag(),
+      gatekeeperNetwork: gatekeeperNetworkFlag(),
+      gatewayNetworkAddress: gatewayNetworkAddressFlag(),
+      chain: chainFlag(),
+      fees: feesFlag(),
+      gasLimit: gasLimitFlag(),
+      confirmations: confirmationsFlag(),
+    };
+  
+    static args = {
+      tokenOwner: Args.string({name: 'tokenOwner', required: true, description: 'Owner of the specified gateway token'}),
+      networkName: Args.string({name: 'networkName', required: true, description: 'Name of the network'}),
+      expirationTimeInSeconds: Args.integer({name: 'expirationTimeInSeconds', required: true, description: "The amount of time in seconds before a token expires. Default value is set to 0. Network primary authority can override this value"}),
+      feeSender: Args.string({name: 'feeSender', required: true, description: "Address that will be paying the fee"}),
+      chargeType: Args.string({name: "chargeType", required: true, description: "Supproted types are: NONE, ETH and ERC20. This is set by the network"}),
+      feeValue: Args.integer({name: "feeValue", required: true, description: "Value charged to fee sender. They must approve before hand"})
+    }
+  
+    async run(): Promise<void> {
+      const {args, flags} = await this.parse(AddGatekeeper)
+  
+      const tokenOwner: string = args.tokenOwner
+      const parsedFlags = parseFlagsWithPrivateKey(flags)
+  
+  
+      const gatewayNetwork = await makeGatewayNetworkTs(parsedFlags)
+      const gatewayTokenTs = await makeGatewayTs(parsedFlags)
+
+      const networkId = await gatewayNetwork.getNetworkId(utils.formatBytes32String(args.networkName));
+      const feeRecipient = (new Wallet(parsedFlags.privateKey)).address
+      const sendableTransaction = await gatewayTokenTs.issue(
+          tokenOwner,
+          networkId.valueOf() as bigint,
+          args.expirationTimeInSeconds,
+          0,
+          {feeRecipient , feeSender: args.feeSender},
+          {
+              tokenSender: args.feeSender,
+              recipient: feeRecipient
+          }
+      )
+  
+      const receipt = await sendableTransaction.wait(flags.confirmations)
+  
+      this.log(
+        `Issued gateway token to use. TxHash: ${receipt.transactionHash}`,
+      )
+    }
+  }
+  
